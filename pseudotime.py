c4435fc80c0014d0ea72541ac1c2cf7142799017
Larry Zhang <lzhang124@gmail.com>
2016-12-04 22:02:53 -0500

    add regression plot

--- pseudotime.py
+++ pseudotime.py
@@ -254,6 +254,8 @@ def plot_pseudotime_trace(gplvm, samples, show_burn=False):
     burn_iter = 0 if show_burn else params["burn"]
     df = pd.DataFrame(gplvm["t_chain"][burn_idx:,samples], index=np.arange(burn_iter, params["n_iter"], params["thin"]))
     df.plot(legend=False)
+    plt.xlabel("Iteration")
+    plt.ylabel("Pseudotime")
     sns.despine()
     plt.show()
 
@@ -263,11 +265,13 @@ def plot_kernel_parameter(gplvm, param, samples):
     params = gplvm["params"]
     df = pd.DataFrame(gplvm[chain_name][params["burn_idx"]:,samples], index=np.arange(params["burn"], params["n_iter"], params["thin"]))
     df.plot(legend=False)
+    plt.xlabel("Iteration")
+    plt.ylabel(param.title())
     sns.despine()
     plt.show()
 
 
-def plot_posterior_estimate(gplvm, X, n, t_real):
+def plot_posterior_estimate(gplvm, X, n, t_true):
     burn_idx = gplvm["params"]["burn_idx"]
     t_vals = np.linspace(0, 1, num=n)
     t_avgs = np.mean(gplvm["t_chain"][burn_idx:,:], axis=0)
@@ -278,9 +282,11 @@ def plot_posterior_estimate(gplvm, X, n, t_real):
     # plot X_p
     scatter = plt.scatter(X_p[:,0], X_p[:,1], s=100, c=t_vals, cmap="RdYlBu", lw=0)
     # plot X
-    plt.scatter(X[:,0], X[:,1], s=75, c=t_real, cmap="RdYlBu")
+    plt.scatter(X[:,0], X[:,1], s=75, c=t_true, cmap="RdYlBu")
     cb = plt.colorbar(scatter)
     cb.outline.set_visible(False)
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
     sns.despine()
     plt.show()
 
@@ -289,6 +295,8 @@ def plot_likelihood(gplvm):
     params = gplvm["params"]
     df = pd.DataFrame(gplvm["likelihood_chain"], index=np.arange(params["burn"], params["n_iter"], params["thin"]))
     df.plot(legend=False)
+    plt.xlabel("Iteration")
+    plt.ylabel("Log Likelihood")
     sns.despine()
     plt.show()
 
@@ -297,36 +305,55 @@ def plot_prior(gplvm):
     params = gplvm["params"]
     df = pd.DataFrame(gplvm["prior_chain"], index=np.arange(params["burn"], params["n_iter"], params["thin"]))
     df.plot(legend=False)
+    plt.xlabel("Iteration")
+    plt.ylabel("Corp Prior")
+    sns.despine()
+    plt.show()
+
+
+def plot_regression(gplvm, t_true):
+    burn_idx = gplvm["params"]["burn_idx"]
+    t_avgs = np.mean(gplvm["t_chain"][burn_idx:,:], axis=0)
+    sns.regplot(x=t_true, y=t_avgs)
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+    plt.xlabel("'True' Pseudotime")
+    plt.ylabel("MAP Pseudotime")
     sns.despine()
     plt.show()
 
 
 #################################################
 
-# Seaborn Setup
-sns.set_style("white")
+def main():
+    # Seaborn Setup
+    sns.set_style("white")
 
-# Synthetic Data
-n = 30
-p = 2
-lambda_ = np.array([1/math.sqrt(2)] * p)
-sigma = np.array([1e-3] * p)
-t_real = np.random.sample(n)
-X = np.zeros((n, p))
-for i in xrange(p):
-    X[:,i] = stats.multivariate_normal.rvs(mean=np.zeros(n), cov=covariance_matrix(t_real, lambda_[i], sigma[i]))
+    # Synthetic Data
+    n = 30
+    p = 2
+    lambda_ = np.array([1/math.sqrt(2)] * p)
+    sigma = np.array([1e-3] * p)
+    t_true = np.random.sample(n)
+    X = np.zeros((n, p))
+    for i in xrange(p):
+        X[:,i] = stats.multivariate_normal.rvs(mean=np.zeros(n), cov=covariance_matrix(t_true, lambda_[i], sigma[i]))
 
-# GPLVM Parameters
-n_iter = 100000
-burn = n_iter/2
-thin = n_iter/100
-t = stats.uniform.rvs(loc=.499, scale=.002, size=n)
-t_var = np.array([.5e-3] * n)
-lambda_var = np.array([.5e-5] * p)
-sigma_var = np.array([.5e-10] * p)
+    # GPLVM Parameters
+    n_iter = 100000
+    burn = n_iter/2
+    thin = n_iter/100
+    t = stats.uniform.rvs(loc=.499, scale=.002, size=n)
+    t_var = np.array([.5e-3] * n)
+    lambda_var = np.array([.5e-5] * p)
+    sigma_var = np.array([.5e-10] * p)
 
-gplvm = GPLVM(X, n_iter, burn, thin, t, t_var, lambda_, lambda_var, sigma, sigma_var)
+    gplvm = GPLVM(X, n_iter, burn, thin, t, t_var, lambda_, lambda_var, sigma, sigma_var)
 
-n_samples = choose_samples(n, n)
-plot_pseudotime_trace(gplvm, n_samples, True)
-plot_posterior_estimate(gplvm, X, 1000, t_real)
+    n_samples = choose_samples(n, n)
+    plot_pseudotime_trace(gplvm, n_samples, True)
+    plot_posterior_estimate(gplvm, X, 1000, t_true)
+    plot_regression(gplvm, t_true)
+
+
+main()
